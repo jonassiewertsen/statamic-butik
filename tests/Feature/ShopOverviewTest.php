ea8bb6c54eece31d9acfbea01f2436a286afb966
Jonas Siewertsen <mail@jonassiewertsen.com>
2020-01-13 09:47:44 +0100

    Basic view setup for the product overview

--- tests/Feature/ShopOverviewTest.php
+++ tests/Feature/ShopOverviewTest.php
@@ -10,7 +10,19 @@ class ShopOverviewTest extends TestCase
     /** @test */
     public function The_shop_overview_page_does_exist()
     {
-        $this->withoutExceptionHandling();
-        $this->get(route('butik.shop'))->assertOk();
+        $this->get(route('butik.shop'))
+            ->assertOk()
+            ->assertViewIs('statamic-butik::web.shop.index');
+    }
+
+    /** @test */
+    public function all_product_information_will_be_shown()
+    {
+        $product = create(Product::class, [], 10)->first();
+
+        $this->get(route('butik.shop'))
+            ->assertSee($product->title)
+            ->assertSee($product->description)
+            ->assertSee($product->base_price);
     }
 }
