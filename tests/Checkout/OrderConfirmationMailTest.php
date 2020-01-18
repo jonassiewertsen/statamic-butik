927f6e0360d158ddee142b447c07496214edcebe
Jonas Siewertsen <mail@jonassiewertsen.com>
2020-01-19 00:24:20 +0100

    Rearranging some tests

--- tests/Checkout/OrderConfirmationMailTest.php
+++ tests/Checkout/OrderConfirmationMailTest.php
@@ -2,17 +2,14 @@
 
 namespace Tests\Checkout;
 
-use Carbon\Carbon;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Mail;
 use Jonassiewertsen\StatamicButik\Mail\OrderConfirmationForCustomer;
-use Jonassiewertsen\StatamicButik\Events\PaymentSuccessful;
-use Jonassiewertsen\StatamicButik\Http\Models\Order;
 use Jonassiewertsen\StatamicButik\Listeners\CreateOrder;
 use Jonassiewertsen\StatamicButik\Mail\OrderConfirmationForSeller;
 use Jonassiewertsen\StatamicButik\Tests\TestCase;
 
-class CreateOrderTest extends TestCase
+class OrderConfirmationMailTest extends TestCase
 {
     public function setUp(): void
     {
@@ -27,41 +24,27 @@ class CreateOrderTest extends TestCase
     }
 
     /** @test */
-    public function a_order_will_be_created(){
+    public function a_purchase_confirmation_mail_will_be_sent_to_the_customer(){
         Event::fake([CreateOrder::class]);
+        Mail::fake();
 
-        $this->makePayment();
+        $amount = $this->makePayment();
 
-        $this->assertCount(1, Order::all());
+        Mail::assertQueued(OrderConfirmationForCustomer::class);
     }
 
     /** @test */
-    public function a_order_will_have_the_braintree_order_id(){
+    public function a_purchase_confirmation_mail_will_be_sent_to_the_seller(){
+        $this->withoutExceptionHandling();
         Event::fake([CreateOrder::class]);
+        Mail::fake();
 
-        $response = $this->makePayment();
-        $id = now()->format('y') .'_'. $response->id;
+        $amount = $this->makePayment();
 
-        $this->assertDatabaseHas('orders', ['id' => $id]);
+        Mail::assertQueued(OrderConfirmationForSeller::class);
     }
 
-    /** @test */
-    public function a_order_will_contain_the_braintree_amount(){
-        Event::fake([CreateOrder::class]);
-        $amount = $this->makePayment()->amount;
-
-        $this->assertDatabaseHas('orders', ['total_amount' => $amount]);
-    }
-
-    /** @test */
-    public function a_order_will_contain_the_braintree_pay_datetime(){
-        Event::fake([CreateOrder::class]);
-        $createdAt = $this->makePayment()->createdAt->date;
-        $createdAt = Carbon::parse($createdAt)->format('Y-m-d H:i:s');
-
-        $this->assertDatabaseHas('orders', ['paid_at' => $createdAt]);
-    }
-
+    // TODO: Move up
     private function makePayment()
     {
         $payload = ['payload' => ['nonce' => 'fake-valid-nonce', 'amount' => mt_rand(0.01, 1999.99)]];
