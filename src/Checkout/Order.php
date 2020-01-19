78cdeb773da6ba5d25193a538479fe72b3e014ea
Jonas Siewertsen <mail@jonassiewertsen.com>
2020-01-19 17:57:29 +0100

    Drafting the order object

---
+++ src/Checkout/Order.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Jonassiewertsen\StatamicButik\Checkout;
+
+use Illuminate\Support\Collection;
+use Jonassiewertsen\StatamicButik\Http\Models\Product;
+
+class Order {
+    public $customer;
+    public $products;
+    public $transaction;
+    public $amount;
+
+    public function __construct()
+    {
+        // TODO: Calculate amount automatically
+    }
+
+    public function customer(Customer $customer) {
+        $this->customer = $customer;
+        return $this;
+    }
+
+    public function transaction(Transaction $transaction) {
+        $this->customer = $transaction;
+        return $this;
+    }
+
+    public function products(Collection $products) {
+        $this->customer = $products;
+        return $this;
+    }
+
+    public function addProduct(Product $product) {
+        if ($this->products->count() === 0) {
+            collect($this->products);
+        }
+        $this->products->push($product);
+    }
+}
