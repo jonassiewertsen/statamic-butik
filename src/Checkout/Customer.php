033cf3246a330584d06c57f743b4f24c7eba69f3
Jonas Siewertsen <mail@jonassiewertsen.com>
2020-01-19 17:59:37 +0100

    drafting a customer object

---
+++ src/Checkout/Customer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Jonassiewertsen\StatamicButik\Checkout;
+
+class Customer {
+    public $name;
+    public $mail;
+    public $address1;
+    public $address2;
+    public $city;
+    public $stateRegion;
+    public $zip;
+    public $phone;
+    public $country;
+}
