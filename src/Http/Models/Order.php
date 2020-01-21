5c44becfe96947ca307f8c9596001519334e542d
Jonas Siewertsen <mail@jonassiewertsen.com>
2020-01-21 23:11:25 +0100

    Order table incrementing anymore

--- src/Http/Models/Order.php
+++ src/Http/Models/Order.php
@@ -4,7 +4,10 @@ namespace Jonassiewertsen\StatamicButik\Http\Models;
 
 class Order extends ButikModel
 {
-    protected $table = 'butik_orders';
+    protected $table        = 'butik_orders';
+    public    $incrementing = false;
+    protected $primaryKey   = 'id';
+    protected $keyType      = 'string';
 
     protected $casts = [
         'products'   => 'array',
