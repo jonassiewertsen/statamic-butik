44c117c5c08c4bcada9da5a71f586c43b84fe1be
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-04-14 20:11:25 +0200

    Clean up web controller

--- src/Http/Controllers/WebController.php
+++ src/Http/Controllers/WebController.php
@@ -6,23 +6,5 @@ use Illuminate\Routing\Controller as LaravelController;
 
 class WebController extends LaravelController
 {
-    protected function addingProductRoutes($product): array {
-        // Todo: This is working, but sucks. Do something about it.
-        return [
-            'title'                => $product->title,
-            'description'          => $product->description,
-            'images'               => $product->images,
-            'base_price'           => $product->base_price,
-            'total_price'          => $product->total_price,
-            'tax_amount'           => $product->tax_amount,
-            'tax_percentage'       => $product->tax_percentage,
-            'shipping_amount'      => $product->shipping_amount,
-            'show_url'             => $product->showUrl,
-            'stock'                => $product->stock,
-            'slug'                 => $product->slug,
-            'stock_unlimited'      => $product->stock_unlimited,
-            'express_delivery_url' => $product->expressDeliveryUrl,
-            'express_payment_url'  => $product->expressPaymentUr,
-        ];
-    }
+   //
 }
