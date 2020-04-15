fe8ac85f8c69846794abe989024947dbf52410a4
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-04-15 22:02:45 +0200

    Add the Cart helper to handle the shopping cart

--- src/Helpers/Cart.php
+++ src/Helpers/Cart.php
@@ -2,49 +2,108 @@
 
 namespace Jonassiewertsen\StatamicButik\Helpers;
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Session;
+use Jonassiewertsen\StatamicButik\Checkout\Item;
 use Jonassiewertsen\StatamicButik\Http\Models\Product;
 
 class Cart {
-    public function __construct()
+    public static $cart;
+
+    /**
+     * A product can be added to the cart
+     */
+    public static function add(Product $product): void
     {
-        if($this->get() === null)
-            $this->set($this->empty());
+        static::$cart = static::get();
+
+        if (self::contains($product)) {
+            // increase the quanitity
+            static::$cart->firstWhere('id', $product->slug)->increase();
+        } else {
+            // Add new Item
+            static::$cart->push(new Item($product));
+        }
+
+        static::set(static::$cart);
     }
 
-    public function add(Product $product): void
+    /**
+     * An item can be reduced or removed from the cart
+     */
+    public static function reduce(Product $product): void
     {
-        $cart = $this->get();
-        array_push($cart['products'], $product);
-        $this->set($cart);
+        static::$cart = static::get();
+
+        static::$cart = static::$cart->filter(function($item) use ($product) {
+            // If the quanitity is higher then one, it will only decrease
+            if ($item->id === $product->slug && $item->quantity > 1) {
+               $item->decrease();
+               return true;
+            }
+
+            return false;
+        });
+
+        static::set(static::$cart);
     }
 
-    public function remove(int $productId): void
+    /**
+     * An item can be completly removed from the cart
+     */
+    public static function remove(Product $product): void
     {
-        $cart = $this->get();
-        array_splice($cart['products'], array_search($productId, array_column($cart['products'], 'id')), 1);
-        $this->set($cart);
+        static::$cart = static::get();
+
+        static::$cart = static::$cart->filter(function($item) use ($product) {
+            return $item->id !== $product->slug;
+        });
+
+        static::set(static::$cart);
     }
 
-    public function clear(): void
-    {
-        $this->set($this->empty());
+    /**
+     * Clear the complete cart
+     */
+    public static function clear(): void {
+        static::set(static::empty());
     }
 
-    public function empty(): array
+    /**
+     * Fetch the cart from the session
+     */
+    public static function get(): Collection
     {
-        return [
-            'products' => [],
-        ];
+        return Session::get('butik.cart') !== null ?
+            Session::get('butik.cart') :
+            static::empty();
     }
 
-    public function get(): ?array
+    /**
+     * Set the cart to the session
+     */
+    private static function set(Collection $cart): void
     {
-        return Session::get('cart');
+        Session::put('butik.cart', $cart);
     }
 
-    private function set($cart): void
+    /**
+     * Is this product already saved in the cart?
+     *
+     * @param Product $product
+     * @return bool
+     */
+    private static function contains(Product $product): bool
     {
-        Session::put('cart', $cart);
+        return static::$cart->contains('id', $product->slug);
+    }
+
+    /**
+     * An empty cart
+     *
+     * @return Collection
+     */
+    private static function empty() {
+        return collect();
     }
 }
--- tests/Unit/CartTest.php
+++ tests/Unit/CartTest.php
@@ -3,82 +3,87 @@
 namespace Tests\Unit;
 
 use Illuminate\Support\Facades\Session;
-use Jonassiewertsen\StatamicButik\Checkout\Cart;
-use Jonassiewertsen\StatamicButik\Checkout\Customer;
-use Jonassiewertsen\StatamicButik\Checkout\Transaction;
+use Jonassiewertsen\StatamicButik\Helpers\Cart;
 use Jonassiewertsen\StatamicButik\Http\Models\Product;
 use Jonassiewertsen\StatamicButik\Tests\TestCase;
 
 class CartTest extends TestCase
 {
-//    protected Cart $cart;
-//    protected Product $product;
-//
-//    public function setUp(): void {
-//        parent::setUp();
-//        $this->cart = new Cart();
-//        $this->product = create(Product::class)->first();
-//    }
-//
-//    /** @test */
-//    public function a_customer_can_be_added()
-//    {
-//        $this->cart->customer(new Customer());
-//        $this->assertInstanceOf(Customer::class, $this->cart->customer);
-//    }
-//
-//    /** @test */
-//    public function a_transaction_can_be_added()
-//    {
-//        $this->cart->transaction(new Transaction());
-//        $this->assertInstanceOf(Transaction::class, $this->cart->transaction);
-//    }
-//
-//    /** @test */
-//    public function a_product_will_be_addes_as_item()
-//    {
-//        $this->cart->add($this->product);
-//        $this->assertCount(1, $this->cart->items);
-//    }
-//
-//    /** @test */
-//    public function a_new_cart_item_has_the_quanitity_of_one()
-//    {
-//        $this->cart->add($this->product);
-//        $this->assertEquals(1, $this->cart->items->first()->quantity);
-//    }
-//
-//    /** @test */
-//    public function an_new_item_will_be_added_to_the_session()
-//    {
-//        $this->cart->add($this->product);
-//
-//        $fromSession = Session::get('butik.cart');
-//
-//        $this->assertEquals(
-//            $fromSession->items->first(),
-//            $this->cart->items->first()
-//        );
-//    }
-//
-//    /** @test */
-//    public function the_quanitity_will_be_increase_if_the_product_already_has_been_added()
-//    {
-//        $this->cart->add($this->product);
-//        $this->assertEquals(1, $this->cart->items->first()->quantity);
-//
-//        $this->cart->add($this->product);
-//        $this->assertEquals(2, $this->cart->items->first()->quantity);
-//    }
-//
-//    /** @test */
-//    public function an_item_can_be_removed()
-//    {
-//        $this->cart->add($this->product);
-//
-//        $fromSession = Session::get('butik.cart');
-//
-//        $this->cart->remove($this->product);
-//        $this->assertFalse($fromSession->items->contains('id', $this->product->slug));
-//    }
+    protected Product $product;
+
+    public function setUp(): void {
+        parent::setUp();
+        $this->product = create(Product::class)->first();
+    }
+
+    /** @test */
+    public function a_product_can_be_addes_as_item()
+    {
+        $this->assertNull(Session::get('butik.cart'));
+
+        Cart::add($this->product);
+
+        $this->assertCount(1, Cart::get());
+    }
+
+    /** @test */
+    public function a_new_cart_item_has_the_quanitity_of_one()
+    {
+        Cart::add($this->product);
+
+        $this->assertEquals(1, Cart::get()->first()->quantity);
+    }
+
+    /** @test */
+    public function the_quanitity_will_be_increase_if_the_product_already_has_been_added()
+    {
+        Cart::add($this->product);
+        $this->assertEquals(1, Cart::get()->first()->quantity);
+
+        Cart::add($this->product);
+        $this->assertEquals(2, Cart::get()->first()->quantity);
+    }
+
+    /** @test */
+    public function an_item_can_be_removed()
+    {
+        Cart::add($this->product);
+        $this->assertTrue(Cart::get()->contains('id', $this->product->slug));
+
+        Cart::reduce($this->product);
+        $this->assertFalse(Cart::get()->contains('id', $this->product->slug));
+    }
+
+    /** @test */
+    public function an_item_with_more_then_one_item_will_only_be_decreased()
+    {
+        Cart::add($this->product);
+        Cart::add($this->product);
+        $this->assertEquals(2, Cart::get()->first()->quantity);
+
+        Cart::reduce($this->product);
+        $this->assertEquals(1, Cart::get()->first()->quantity);
+    }
+
+    /** @test */
+    public function an_item_can_be_completly_removed()
+    {
+        Cart::add($this->product);
+        Cart::add($this->product);
+        $this->assertEquals(2, Cart::get()->first()->quantity);
+
+        Cart::remove($this->product);
+        $this->assertFalse(Cart::get()->contains('id', $this->product->slug));
+    }
+
+    /** @test */
+    public function the_cart_can_be_cleared()
+    {
+        Cart::add($this->product);
+        Cart::add($this->product);
+        $this->assertEquals(2, Cart::get()->first()->quantity);
+
+        Cart::clear();
+        $this->assertTrue((Cart::get() == collect()));
+    }
 }
