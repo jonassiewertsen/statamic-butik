fc19f85b76c89dc38eb9ac85ae7f10c161fed593
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-04-16 22:21:59 +0200

    Optimize card layout

--- resources/views/web/cart/partials/card.blade.php
+++ resources/views/web/cart/partials/card.blade.php
@@ -2,21 +2,24 @@
     <header class="b-w-40 b-w-1/5">
         @include('butik::web.shop.partials.placeholder')
     </header>
-    <section class="b-w-4/5 b-ml-12">
-        <h3 class="b-font-bold b-block b-mt-5 b-text-2xl">{{ $item->name }}</h3>
+    <section class="b-flex b-flex-col b-justify-between b-ml-12 b-w-4/5">
+        <div>
+            <h3 class="b-font-bold b-block b-mt-5 b-text-2xl">{{ $item->name }}</h3>
 
-        <hr class="b-border-white b-my-3">
+            @if ($item->product->description)
+                <hr class="b-border-white b-my-3">
+                <p>{{ $item->product->description }}</p>
+                <hr class="b-border-white b-my-3">
+            @endif
 
-        <p>Aufblasbares Stand Up Paddle von Indiana Jahrgang 2019 gebraucht in super Zustand mit Kids Paddel, Tasche, Finne, Pumpe und Rep Kit kann gerne vor Ort getestet werden</p>
-
-        <hr class="b-border-white b-my-3">
+        </div>
 
         <footer class="b-flex b-justify-end b-items-center">
             <span class="">$ 236</span>
-            <figure class="b-ml-10">
-                <button wire:click="reduce('{{ $item->id }}')" class="b-bg-gray-400 b-px-4 b-py-1 b-rounded-full">-</button>
-                <span class="b-bg-gray-400 b-px-4 b-py-1 b-rounded-full">{{ $item->quantity }}</span>
-                <button wire:click="add('{{ $item->id }}')" class="b-bg-gray-400 b-px-4 b-py-1 b-rounded-full">+</button>
+            <figure class="b-flex b-items-center b-ml-10">
+                <button wire:click="reduce('{{ $item->id }}')" class="b-bg-gray-300 b-flex b-font-bold b-h-8 b-justify-center b-py-1 b-rounded-full b-text-gray-600 b-w-8 hover:b-bg-gray-400">-</button>
+                <span class="b-px-3">{{ $item->quantity }}</span>
+                <button wire:click="add('{{ $item->id }}')" class="b-bg-gray-300 b-flex b-font-bold b-h-8 b-justify-center b-py-1 b-rounded-full b-text-gray-600 b-w-8 hover:b-bg-gray-400">+</button>
             </figure>
             <strong class="b-ml-10">$ 236</strong>
         </footer>
