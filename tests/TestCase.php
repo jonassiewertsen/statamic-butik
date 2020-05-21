fd771fc6e6e8ac2c714bf2fd0432ab38d2fb27f8
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-05-21 12:27:11 +0200

    Remove the manual added ExceptionHandler. Seems to not be needed anymore

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -21,8 +21,6 @@ class TestCase extends OrchestraTestCase
      */
     protected function setUp(): void
     {
-        require_once(__DIR__.'/ExceptionHandler.php');
-
         parent::setUp();
 
         if ($this->shouldFakeVersion) {
