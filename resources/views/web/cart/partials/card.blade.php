1dbb4fffa1295e8c97bfea1b79553975ccc96a1e
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-06-28 11:42:41 +0200

    Make visible inside the cart, if a product can't bought because it's not avialbe in the choosen country

--- resources/views/web/cart/partials/card.blade.php
+++ resources/views/web/cart/partials/card.blade.php
@@ -1,4 +1,4 @@
-<div class="b-bg-gray-100 b-flex b-w-full b-rounded b-px-8 b-py-5 b-mb-6">
+<div class="{{ $item->buyable ? 'b-bg-gray-100' : 'b-bg-red-200' }} b-flex b-w-full b-rounded b-px-8 b-py-5 b-mb-6">
     <header class="b-w-40 b-w-1/5">
         @include('butik::web.shop.partials.placeholder')
     </header>
@@ -7,7 +7,11 @@
             <h3 class="b-font-bold b-block b-mt-5 b-text-2xl">{{ $item->name }}</h3>
             @if ($item->description)
                 <hr class="b-border-white b-my-3">
-                <p>{{ $item->description }}</p>
+                @if ($item->buyable)
+                    <p>{{ $item->description }}</p>
+                @else
+                    <p>{{ __('butik::cart.not_available_in_country') }}</p>
+                @endif
                 <hr class="b-border-white b-my-3">
             @endif
 
@@ -20,7 +24,13 @@
                 <span class="b-px-3">{{ $item->getQuantity() }}</span>
                 <button wire:click="add('{{ $item->id }}')" class="b-bg-gray-300 b-flex b-font-bold b-h-8 b-justify-center b-py-1 b-rounded-full b-text-gray-600 b-w-8 hover:b-bg-gray-400">+</button>
             </figure>
-            <strong class="b-ml-10">{{ currency() }} {{ $item->totalPrice() }}</strong>
+            <strong class="b-ml-10">
+                @if ($item->buyable)
+                    {{ currency() }} {{ $item->totalPrice() }}
+                @else
+                    <s>{{ currency() }} {{ $item->totalPrice() }}</s>
+                @endif
+            </strong>
         </footer>
     </section>
 </div>
