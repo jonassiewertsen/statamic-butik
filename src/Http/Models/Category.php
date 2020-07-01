4eb38cb3eef229d9bb596d8cd89a7848bd98bb0c
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-07-01 23:43:42 +0200

    Products can be attached and detached to categories

--- src/Http/Models/Category.php
+++ src/Http/Models/Category.php
@@ -2,11 +2,13 @@
 
 namespace Jonassiewertsen\StatamicButik\Http\Models;
 
+use Illuminate\Support\Facades\DB;
+
 class Category extends ButikModel
 {
-    protected $table        = 'butik_categories';
-    protected $primaryKey   = 'slug';
-    protected $keyType      = 'string';
+    protected $table      = 'butik_categories';
+    protected $primaryKey = 'slug';
+    protected $keyType    = 'string';
 
     protected $guarded = [];
 
@@ -21,6 +23,45 @@ class Category extends ButikModel
 
     public function products()
     {
-        return $this->belongsToMany(Category::class, 'butik_category_product');
+        return $this->belongsToMany(
+            Category::class,
+            'butik_category_product',
+            'category_slug',
+            'product_slug'
+        );
+    }
+
+    /**
+     * Add a product to this category
+     */
+    public function addProduct(Product $product): void
+    {
+        $this->products()->attach($product);
+    }
+
+    /**
+     * Add a product to this category
+     */
+    public function removeProduct(Product $product): void
+    {
+        $this->products()->detach($product);
+    }
+
+    /**
+     * Is a specific product attached to this category?
+     */
+    public function isAttachedTo(Product $product): bool
+    {
+        return $this->isTheProductAttachedToThisCagegory($product);
+    }
+
+    /**
+     * Is exactly one entry saved in the butik_category_product table?
+     */
+    private function isTheProductAttachedToThisCagegory(Product $product): bool
+    {
+        return DB::table('butik_category_product')
+                ->where('product_slug', $product->slug)
+                ->count() === 1;
     }
 }
--- tests/Unit/CategoryTest.php
+++ tests/Unit/CategoryTest.php
@@ -3,14 +3,50 @@
 namespace Tests\Unit;
 
 use Jonassiewertsen\StatamicButik\Http\Models\Category;
+use Jonassiewertsen\StatamicButik\Http\Models\Product;
 use Jonassiewertsen\StatamicButik\Tests\TestCase;
 
 class CategoryTest extends TestCase
 {
+    public ?Product $product;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->product = create(Product::class)->first();
+    }
+
     /** @test */
-    public function it_is_available_as_default(){
+    public function it_is_available_as_default()
+    {
         $category = create(Category::class)->first();
 
         $this->assertInstanceOf('Illuminate\Support\Collection', $category->products);
+    }
+
+    /** @test */
+    public function it_can_be_attached_to_a_product()
+    {
+        create(Category::class);
+        $category = Category::first();
+
+        $this->assertFalse($category->isAttachedTo($this->product));
+
+        $category->addProduct($this->product);
+        $this->assertTrue($category->isAttachedTo($this->product));
+    }
+
+    /** @test */
+    public function it_can_be_detatched_to_a_product()
+    {
+        create(Category::class);
+        $category = Category::first();
+        $category->addProduct($this->product);
+
+        $this->assertTrue($category->isAttachedTo($this->product));
+
+        $category->removeProduct($this->product);
+        $this->assertFalse($category->isAttachedTo($this->product));
     }
 }
