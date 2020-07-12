2c3cdf57e59bb00c720748dc8005cfc862f4b6d5
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-07-12 20:45:25 +0200

    The stock will be reduced, respecting the variant settings if the stock should be inherited or not. Closes #39

--- tests/Checkout/ProductStockTest.php
+++ tests/Checkout/ProductStockTest.php
@@ -7,6 +7,7 @@ use Jonassiewertsen\StatamicButik\Checkout\Item;
 use Jonassiewertsen\StatamicButik\Checkout\Transaction;
 use Jonassiewertsen\StatamicButik\Http\Models\Order;
 use Jonassiewertsen\StatamicButik\Http\Models\Product;
+use Jonassiewertsen\StatamicButik\Http\Models\Variant;
 use Jonassiewertsen\StatamicButik\Tests\TestCase;
 use Jonassiewertsen\StatamicButik\Tests\Utilities\MolliePaymentSuccessful;
 use Mollie\Laravel\Facades\Mollie;
@@ -58,6 +59,62 @@ class ProductStockTest extends TestCase
         $this->post(route('butik.payment.webhook.mollie'), ['id' => $order->id]);
 
         $this->assertEquals($stock - 2, $product->fresh()->stock);
+    }
+
+    /** @test */
+    public function the_variant_stock_will_be_reduced_by_the_items_quantity_after_checkout()
+    {
+        $variant = create(Variant::class, ['inherit_stock' => false])->first();
+        $stock   = $variant->stock;
+
+        $item = (new Item($variant->slug));
+        $item->setQuantity(2);
+
+        $transaction = (new Transaction())->items(collect()->push($item));
+
+        $order = create(Order::class, [
+            'transaction_id' => 'tr_fake_id',
+            'items'          => json_encode($transaction->items),
+        ])->first();
+
+        $this->assertEquals($stock, Variant::first()->stock);
+
+        $this->mockMollie(new MolliePaymentSuccessful());
+        $this->post(route('butik.payment.webhook.mollie'), ['id' => $order->id]);
+
+        $this->assertEquals($stock - 2, $variant->fresh()->stock);
+    }
+
+    /** @test */
+    public function the_parent_stock_will_be_reduced_if_the_stock_is_inherited()
+    {
+        $product = create(Product::class)->first();
+        $variant = create(Variant::class, [
+            'inherit_stock' => true,
+            'product_slug'   => $product->slug,
+        ])->first();
+
+        $productStock = $product->stock;
+        $variantStock = $variant->original_stock;
+
+        $item = (new Item($variant->slug));
+        $item->setQuantity(2);
+
+        $transaction = (new Transaction())->items(collect()->push($item));
+
+        $order = create(Order::class, [
+            'transaction_id' => 'tr_fake_id',
+            'items'          => json_encode($transaction->items),
+        ])->first();
+
+        $this->assertEquals($productStock, Product::first()->stock);
+        $this->assertEquals($variantStock, Variant::first()->original_stock);
+
+        $this->mockMollie(new MolliePaymentSuccessful());
+        $this->post(route('butik.payment.webhook.mollie'), ['id' => $order->id]);
+
+        $this->assertEquals($variantStock, Variant::first()->original_stock);
+        $this->assertEquals($productStock - 2, Product::first()->stock);
     }
 
     /** @test */
