5fe5c276846951afa7dd9086a9651d3b9baed767
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-07-16 20:43:11 +0200

    Fix translation key

--- resources/lang/en/cp.php
+++ resources/lang/en/cp.php
@@ -6,7 +6,7 @@ return [
     'all_informations_saved_from_buying_date' => 'All Informations saved from buying date',
     'available'                               => 'available',
     'city'                                    => 'city',
-    'configure_after_your_needs.'             => 'Configure Butik after your needs.',
+    'configure_after_your_needs'              => 'Configure Butik after your needs.',
     'country_singular'                        => 'country',
     'country_description'                     => 'Countries will be used to define the countries, you will sell you products to.',
     'country_plural'                          => 'countries',
