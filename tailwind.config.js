a2bac6f9ce4986f161003308bc249128172ce468
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-07-19 17:39:43 +0200

    Update css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   purge: false,
   theme: {
+    colors: {
+      accent: '#0DCCAC',
+      dark: '#2D3748',
+      gray: '#718096',
+      'gray-light': '#EDF2F7',
+      white: '#FFFFFF',
+    },
     extend: {
       borderRadius: {
         'xl': '12px',
