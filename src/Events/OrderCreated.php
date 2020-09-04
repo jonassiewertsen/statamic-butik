d2ed2b4889b4315aa133d70795a67638bb00fbc7
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-09-04 21:55:57 +0200

    Clean up orderCreated event

--- src/Events/OrderCreated.php
+++ src/Events/OrderCreated.php
@@ -3,14 +3,11 @@
 namespace Jonassiewertsen\StatamicButik\Events;
 
 use Illuminate\Queue\SerializesModels;
-use Jonassiewertsen\StatamicButik\Checkout\Transaction;
 use Jonassiewertsen\StatamicButik\Http\Models\Order;
 
 class OrderCreated
 {
     use SerializesModels;
-
-    public Transaction $transaction;
 
     public function __construct(Order $order)
     {
