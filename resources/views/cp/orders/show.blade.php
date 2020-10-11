6b15ff25b02e830c28ad3afe8f632b729940f489
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-10-11 23:00:35 +0200

    Additional customer information will automatically be shown in the backend.

--- resources/views/cp/orders/show.blade.php
+++ resources/views/cp/orders/show.blade.php
@@ -6,11 +6,11 @@
     <div class="card p-0">
         <table class="data-table">
             <tr>
-                <th class="pl-2 py-1 w-1/4">{{ __('butik::cp.id') }}</th>
+                <th class="pl-2 py-1 w-1/4">{{ __('butik::cp.order_singular') }} {{ __('butik::cp.id') }}</th>
                 <td>{{ $order->id }}</td>
             </tr>
             <tr>
-                <th class="pl-2 py-1 w-1/4">{{ __('butik::cp.order_number') }}</th>
+                <th class="pl-2 py-1 w-1/4">{{ __('butik::cp.order_singular') }} {{ __('butik::cp.order_number') }}</th>
                 <td>{{ $order->number }}</td>
             </tr>
             <tr>
@@ -75,6 +75,12 @@
                 <th class="pl-2 py-1 w-1/4">{{ __('butik::cp.country_singular') }}</th>
                 <td>{{ $customer->country }}</td>
             </tr>
+             @foreach($additionalCustomerInformation as $information)
+                <tr>
+                    <th class="pl-2 py-1 w-1/4">{{ $information['name']  }}</th>
+                    <td>{{ $information['value'] }}</td>
+                </tr>
+            @endforeach
         </table>
     </div>
 
--- src/Http/Controllers/CP/OrdersController.php
+++ src/Http/Controllers/CP/OrdersController.php
@@ -5,6 +5,7 @@ namespace Jonassiewertsen\StatamicButik\Http\Controllers\CP;
 use Jonassiewertsen\StatamicButik\Http\Controllers\CpController;
 use Jonassiewertsen\StatamicButik\Http\Models\Order;
 use Statamic\CP\Column;
+use Statamic\Support\Str;
 
 class OrdersController extends CpController
 {
@@ -46,9 +47,30 @@ class OrdersController extends CpController
     {
         $this->authorize('show', $order);
 
-        $customer = $order->customer;
-        $items    = $order->items;
+        $customer                      = $order->customer;
+        $additionalCustomerInformation = $this->extractAdditionalInformation($customer);
+        $items                         = $order->items;
 
-        return view('butik::cp.orders.show', compact('order', 'customer', 'items'));
+        return view('butik::cp.orders.show', compact('order', 'customer', 'additionalCustomerInformation', 'items'));
+    }
+
+    /**
+     * We will only return those values, which aren't default values.
+     */
+    private function extractAdditionalInformation(\stdClass $customer)
+    {
+        $defaultValues  = ['firstname', 'surname', 'email', 'address1', 'address2', 'city', 'zip', 'country'];
+
+        return collect($customer)->filter(function ($value, $key) use ($defaultValues) {
+            // Filtering if additinal values do exist.
+            return ! in_array($key, $defaultValues);
+        })->map(function ($value, $key) {
+            // Returning additional values with a converted name.
+            // tax_id will become to Tax Id
+            return [
+                'name'  => (string)Str::of($key)->replace('_', ' ')->title(),
+                'value' => $value,
+            ];
+        });
     }
 }
