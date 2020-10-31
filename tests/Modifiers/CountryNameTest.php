1b4a033173a36b391ef0ced0571f4d798cd2d02c
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-10-31 14:12:26 +0100

    Refactor modifier tests

--- tests/Modifiers/CountryNameTest.php
+++ tests/Modifiers/CountryNameTest.php
@@ -6,16 +6,26 @@ use Jonassiewertsen\StatamicButik\Http\Models\ShippingZone;
 use Jonassiewertsen\StatamicButik\Modifiers\CountryName;
 use Jonassiewertsen\StatamicButik\Shipping\Country;
 use Jonassiewertsen\StatamicButik\Tests\TestCase;
+use Statamic\Modifiers\Modifier;
 
 class CountryNameTest extends TestCase
 {
+    protected Modifier $modifier;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->modifier = new CountryName();
+    }
+
     /** @test */
     public function a_variant_can_be_added_as_item()
     {
         create(ShippingZone::class);
 
         $this->assertEquals(
-            (new CountryName())->index(Country::get()),
+            $this->modifier->index(Country::get()),
             Country::list()->first()
         );
     }
--- tests/Modifiers/SellableTest.php
+++ tests/Modifiers/SellableTest.php
@@ -6,16 +6,26 @@ use Jonassiewertsen\StatamicButik\Checkout\Item;
 use Jonassiewertsen\StatamicButik\Modifiers\Sellable;
 
 use Jonassiewertsen\StatamicButik\Tests\TestCase;
+use Statamic\Modifiers\Modifier;
 
 class SellableTest extends TestCase
 {
+    protected Modifier $modifier;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->modifier = new Sellable();
+    }
+
     /** @test */
     public function a_product_can_be_sellable()
     {
         $product = $this->makeProduct();
         $item = new Item($product->slug);
 
-        $this->assertTrue((new Sellable())->index([(array) $item], null, null));
+        $this->assertTrue($this->modifier->index([(array) $item], null, null));
     }
 
     /** @test */
@@ -25,6 +35,6 @@ class SellableTest extends TestCase
         $item = new Item($product->slug);
         $item->nonSellable();
 
-        $this->assertFalse((new Sellable())->index([(array) $item], null, null));
+        $this->assertFalse($this->modifier->index([(array) $item], null, null));
     }
 }
