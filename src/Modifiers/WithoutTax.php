1ec7258a4af83476053e9c590be14aa115b3263b
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-10-31 13:00:34 +0100

    Create empty withoutTax modifier

---
+++ src/Modifiers/WithoutTax.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Jonassiewertsen\StatamicButik\Modifiers;
+
+use Statamic\Modifiers\Modifier;
+
+class WithoutTax extends Modifier
+{
+    public function index($values, $params, $context)
+    {
+
+    }
+}
