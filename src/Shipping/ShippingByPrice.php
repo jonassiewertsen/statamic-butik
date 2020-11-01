baac01f15d52a33608c279790235436973b40a31
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-11-01 13:56:30 +0100

    Refactor the shipping class to make it more easy to add it's own shipping classes

--- src/Shipping/ShippingByPrice.php
+++ src/Shipping/ShippingByPrice.php
@@ -2,65 +2,14 @@
 
 namespace Jonassiewertsen\StatamicButik\Shipping;
 
-use Jonassiewertsen\StatamicButik\Http\Models\ShippingRate;
-use Jonassiewertsen\StatamicButik\Http\Models\ShippingZone;
-use Jonassiewertsen\StatamicButik\Http\Traits\MoneyTrait;
-
 class ShippingByPrice extends ShippingType
 {
-    use MoneyTrait;
-
-    public ShippingZone $zone;
-    public ShippingRate $rate;
-    public int          $totalItemValue;
-
-    public function __construct()
-    {
-        parent::__construct();
-
-        $this->totalItemValue = 0;
-    }
-
-    public function calculate(): ShippingAmount
-    {
-        $this->calculateSummedItemValue();
-        $this->detectShippingRate($this->zone);
-
-        return new ShippingAmount(
-            $this->shippingCosts(),
-            $this->zone->profile,
-            $this->rate,
-            $this->zone->tax,
-        );
-    }
-
     /**
-     * Which shipping rate is the correct one for our
-     * summed total values of all items?
+     * The shipping costs are equal to the shipping price
+     * of the selected shipping rate.
      */
-    protected function detectShippingRate($zone): void
+    public function shippingCosts(): string
     {
-        // As the first step, we will only keep rates, where
-        // the total item value is bigger or equal
-        // as the minimum value of the shipping rate.
-        $rates = $zone->rates->filter(function ($zone) {
-            return $this->totalItemValue >= $this->convertIntoCents($zone->minimum);
-        });
-
-        // Making sure to select the correct rate, we
-        // will sort them and select the first one.
-        $this->rate = $rates->sortByDesc('minimum')->first();
-    }
-
-    /**
-     * We need to calculate the item values, so we can detect the correct
-     * Shipping Rates later. This step is dedicated to the shipping
-     * type calculated via the bag price.
-     */
-    private function calculateSummedItemValue(): void
-    {
-        $this->items->each(function ($item) {
-            $this->totalItemValue += $this->makeAmountSaveable($item->totalPrice());
-        });
+        return $this->rate->price;
     }
 }
