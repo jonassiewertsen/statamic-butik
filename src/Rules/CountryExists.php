94d1b550f0cf536cf38e9813b7f6fc53a4902161
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-12-04 00:23:31 +0100

    Refactor the country exsits rule

---
+++ src/Rules/CountryExists.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Jonassiewertsen\StatamicButik\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Symfony\Component\Intl\Countries;
+
+class CountryExists implements Rule
+{
+    protected string $countryCode;
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param string $attributes
+     * @param mixed $values
+     * @return bool
+     */
+    public function passes($attribute, $values)
+    {
+        foreach ($values as $countryCode) {
+            if (! Countries::exists($countryCode)) {
+                $this->countryCode = $countryCode;
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'Invalid country code: ' . $this->countryCode;
+    }
+}
---
+++ tests/Rules/CountryExistsTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Rules;
+
+use Jonassiewertsen\StatamicButik\Rules\CountryExists;
+use Jonassiewertsen\StatamicButik\Tests\TestCase;
+
+class CountryExistsTest extends TestCase
+{
+    /** @test */
+    public function will_return_true_if_the_country_exists()
+    {
+        $rule = new CountryExists();
+
+        $this->assertTrue($rule->passes('attribute', ['DE']));
+    }
+
+    /** @test */
+    public function it_can_validate_multiple_country_codes()
+    {
+        $rule = new CountryExists();
+
+        $this->assertTrue($rule->passes('attribute', ['DE', 'DK', 'GB']));
+    }
+
+    /** @test */
+    public function it_will_detect_a_false_code_inside_existing_codes()
+    {
+        $rule = new CountryExists();
+
+        $this->assertFalse($rule->passes('attribute', ['DE', 'not-existing', 'DK']));
+    }
+}
