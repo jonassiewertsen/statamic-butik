6b2f9dc3be8c6e96da7b04615275b2b74e0f3ec3
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-12-04 01:04:10 +0100

    Formatting

--- src/Http/Models/ShippingProfile.php
+++ src/Http/Models/ShippingProfile.php
@@ -4,7 +4,7 @@ namespace Jonassiewertsen\StatamicButik\Http\Models;
 
 class ShippingProfile extends ButikModel
 {
-    public    $incrementing = false;
+    public $incrementing = false;
     protected $table = 'butik_shipping_profiles';
     protected $primaryKey = 'slug';
     protected $keyType = 'string';
