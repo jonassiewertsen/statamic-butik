b0a404a1481655dcb5f609dc3317bf6ba023f731
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2021-04-05 12:37:14 +0200

    Add IDE Support for facades

--- src/Facades/Tax.php
+++ src/Facades/Tax.php
@@ -2,9 +2,31 @@
 
 namespace Jonassiewertsen\Butik\Facades;
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade;
+use Jonassiewertsen\Butik\Contracts\PriceCalculator;
+use Jonassiewertsen\Butik\Contracts\ProductRepository;
+use Jonassiewertsen\Butik\Contracts\TaxCalculator;
 use Jonassiewertsen\Butik\Contracts\TaxRepository;
+use Statamic\Contracts\Entries\QueryBuilder;
 
+/**
+ * @method static Collection all()
+ * @method static self|null find(string $id)
+ * @method static self|null findBySlug(string $slug)
+ * @method static bool exists(string $slug)
+ * @method static QueryBuilder query(string $slug)
+ * @method static bool update(array $data)
+ * @method static bool delete(string $id)
+ * @method static string collection()
+ * @method static array toArray()
+ *
+ * @method static float|int rate()
+ * @method static string type()
+ * @method static TaxRepository|null for(ProductRepository $product, string|null $locale = null)
+ *
+ * @see \Jonassiewertsen\Butik\Repositories\TaxRepository
+ */
 class Tax extends Facade
 {
     protected static function getFacadeAccessor(): string
--- src/Facades/Vat.php
+++ src/Facades/Vat.php
@@ -3,8 +3,17 @@
 namespace Jonassiewertsen\Butik\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Jonassiewertsen\Butik\Contracts\PriceRepository;
 use Jonassiewertsen\Butik\Contracts\VatCalculator;
 
+/**
+ * @method static self of(mixed $amount)
+ * @method static self withRate(float|int $rate)
+ * @method static PriceRepository toNet()
+ * @method static PriceRepository toGross()
+ *
+ * @see \Jonassiewertsen\Butik\Support\Vat
+ */
 class Vat extends Facade
 {
     protected static function getFacadeAccessor(): string
