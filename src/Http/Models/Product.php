05db2fcf10f31c99a3d5d77b4230997343b65a7c
Jonas Siewertsen <mail@jonassiewertsen.com>
2021-06-11 23:41:40 +0200

    Formatting

--- src/Http/Models/Product.php
+++ src/Http/Models/Product.php
@@ -276,7 +276,7 @@ class Product
                 $product->$key = $entry->augmentedValue($key);
             }
         }
-        
+
         // Remove variants from the product object, as variants will be called over the __get method.
         // This will be solved differently in butik version 4.
         unset($product->variants);
