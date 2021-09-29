cd60eb52610d56766643bb809eeb2008da8a9f53
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2021-09-29 21:29:34 +0200

    Make categories available for products

--- src/Http/Models/Product.php
+++ src/Http/Models/Product.php
@@ -155,6 +155,20 @@ class Product
     }
 
     /**
+     * Map categories to an array an only return the slug and name, as needed for the Antlers view.
+     */
+    public function mapCategories(self $product)
+    {
+        return $product->categories()
+            ->map(function ($category) {
+                return [
+                    'name' => $category->name,
+                    'slug' => $category->slug,
+                ];
+            })->all();
+    }
+
+    /**
      * A Product has a shipping relation.
      */
     public function shippingProfile()
@@ -289,6 +303,7 @@ class Product
         $product->stock_unlimited = (bool) $entry->value('stock_unlimited');
         $product->available = (bool) $entry->published();
         $product->show_url = $product->showUrl($product->slug);
+        $product->categories = $this->mapCategories($product);
 
         return $product;
     }
